f2887ae9cb6646fcced3b300b06aca911aa99b4e
Velentyna Koriahina <v.koriagina.work@gmail.com>
2024-05-20 14:55:27 +0100

    Back button

--- src/pages/exerciseDetails/components/ExerciseDetails/ExerciseDetails.jsx
+++ src/pages/exerciseDetails/components/ExerciseDetails/ExerciseDetails.jsx
@@ -334,8 +334,209 @@
 
 // export default ExerciseDetails;
 
+// import React, { useState, useEffect } from 'react';
+// import { useParams } from 'react-router-dom';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { editExercise } from 'app/actions/exercise';
+// import Button from 'components/Button/Button';
+// import EditIcon from '../icons/EditIcon';
+// import NotificationSnackbar from '../NotificationSnackbar';
+
+// const ExerciseDetails = ({ action }) => {
+//   const { id } = useParams();
+//   const entity = useSelector(state => state.exercises.exercises.find(e => e.id === parseInt(id, 10)));
+//   const dispatch = useDispatch();
+//   const [isEditing, setIsEditing] = useState(action === 'edit');
+//   const [notification, setNotification] = useState({ open: false, message: '', severity: '' }); // Добавляем состояние уведомлений
+
+//   const [formData, setFormData] = useState({
+//     topic: '',
+//     difficultyRange: '',
+//     studentLevel: '',
+//     mode: '',
+//     type: '',
+//     tags: '',
+//     solutionStrategy: '',
+//     PGN: '',
+//     targetSkills: '',
+//     rate: ''
+//   });
+
+//   useEffect(() => {
+//     if (entity) {
+//       setFormData({
+//         topic: entity.topic,
+//         difficultyRange: entity.difficultyRange,
+//         studentLevel: entity.studentLevel,
+//         mode: entity.mode,
+//         type: entity.type,
+//         tags: entity.tags.join(', '),
+//         solutionStrategy: entity.solutionStrategy,
+//         PGN: entity.PGN,
+//         targetSkills: entity.targetSkills,
+//         rate: entity.rate
+//       });
+//     } else {
+//       setFormData({
+//         topic: '',
+//         difficultyRange: '',
+//         studentLevel: '',
+//         mode: '',
+//         type: '',
+//         tags: '',
+//         solutionStrategy: '',
+//         PGN: '',
+//         targetSkills: '',
+//         rate: ''
+//       });
+//     }
+//   }, [entity]);
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setFormData({ ...formData, [name]: value });
+//   };
+
+//   const handleSave = () => {
+//     const updatedEntity = {
+//       ...entity,
+//       ...formData,
+//       tags: formData.tags.split(',').map(tag => tag.trim())
+//     };
+//     dispatch(editExercise(updatedEntity))
+//       .then(() => {
+//         setIsEditing(false);
+//         setNotification({ open: true, message: 'Зміни збережено', severity: 'success' }); // Устанавливаем уведомление
+//       })
+//       .catch(error => {
+//         console.error('Failed to save exercise:', error);
+//         setNotification({ open: true, message: 'Помилка при збереженні', severity: 'error' }); // Устанавливаем уведомление об ошибке
+//       });
+//   };
+
+//   const handleCancel = () => {
+//     setIsEditing(false);
+//     if (entity) {
+//       setFormData({
+//         topic: entity.topic,
+//         difficultyRange: entity.difficultyRange,
+//         studentLevel: entity.studentLevel,
+//         mode: entity.mode,
+//         type: entity.type,
+//         tags: entity.tags.join(', '),
+//         solutionStrategy: entity.solutionStrategy,
+//         PGN: entity.PGN,
+//         targetSkills: entity.targetSkills,
+//         rate: entity.rate
+//       });
+//     } else {
+//       setFormData({
+//         topic: '',
+//         difficultyRange: '',
+//         studentLevel: '',
+//         mode: '',
+//         type: '',
+//         tags: '',
+//         solutionStrategy: '',
+//         PGN: '',
+//         targetSkills: '',
+//         rate: ''
+//       });
+//     }
+//   };
+
+//   const handleCloseNotification = () => {
+//     setNotification({ ...notification, open: false });
+//   };
+
+//   if (id && !entity) {
+//     return <div>Задачу не знайдено</div>;
+//   }
+
+//   return (
+//     <div className="exercise-details">
+//       <div className="content-container" style={{ position: 'relative' }}>
+//         <div>
+//           {isEditing ? (
+//             <div>
+//               <h1>{id ? `Редагувати задачу` : 'Створити задачу'}</h1>
+//               <form>
+//                 <div>
+//                   <label>Тема:</label>
+//                   <input name="topic" value={formData.topic} onChange={handleChange} />
+//                 </div>
+//                 <div>
+//                   <label>Сложность:</label>
+//                   <input name="difficultyRange" value={formData.difficultyRange} onChange={handleChange} />
+//                 </div>
+//                 <div>
+//                   <label>Уровень студента:</label>
+//                   <input name="studentLevel" value={formData.studentLevel} onChange={handleChange} />
+//                 </div>
+//                 <div>
+//                   <label>Режим:</label>
+//                   <input name="mode" value={formData.mode} onChange={handleChange} />
+//                 </div>
+//                 <div>
+//                   <label>Тип:</label>
+//                   <input name="type" value={formData.type} onChange={handleChange} />
+//                 </div>
+//                 <div>
+//                   <label>Теги:</label>
+//                   <input name="tags" value={formData.tags} onChange={handleChange} />
+//                 </div>
+//                 <div>
+//                   <label>Стратегия решения:</label>
+//                   <input name="solutionStrategy" value={formData.solutionStrategy} onChange={handleChange} />
+//                 </div>
+//                 <div>
+//                   <label>PGN:</label>
+//                   <input name="PGN" value={formData.PGN} onChange={handleChange} />
+//                 </div>
+//                 <div>
+//                   <label>Целевые навыки:</label>
+//                   <input name="targetSkills" value={formData.targetSkills} onChange={handleChange} />
+//                 </div>
+//                 <div>
+//                   <label>Оценка:</label>
+//                   <input name="rate" value={formData.rate} onChange={handleChange} />
+//                 </div>
+//                 <Button onClick={handleSave}>Зберегти</Button>
+//                 <Button onClick={handleCancel}>Скасувати</Button>
+//               </form>
+//             </div>
+//           ) : (
+//             <div>
+//               <button className="edit-button" style={{ position: 'absolute', top: '10px', right: '10px' }} onClick={() => setIsEditing(true)}>
+//                 <EditIcon />
+//               </button>
+//               <h1>{entity.topic}</h1>
+//               <p><strong>Сложность:</strong> {entity.difficultyRange}</p>
+//               <p><strong>Уровень студента:</strong> {entity.studentLevel}</p>
+//               <p><strong>Режим:</strong> {entity.mode}</p>
+//               <p><strong>Тип:</strong> {entity.type}</p>
+//               <p><strong>Теги:</strong> {entity.tags.join(', ')}</p>
+//               <p><strong>Стратегия решения:</strong> {entity.solutionStrategy}</p>
+//               <pre><strong>PGN:</strong> {entity.PGN}</pre>
+//               <p><strong>Целевые навыки:</strong> {entity.targetSkills}</p>
+//               <p><strong>Оценка:</strong> {entity.rate}</p>
+//             </div>
+//           )}
+//         </div>
+//       </div>
+//       <NotificationSnackbar
+//         open={notification.open}
+//         message={notification.message}
+//         severity={notification.severity}
+//         onClose={handleCloseNotification}
+//       />
+//     </div>
+//   );
+// };
+
+// export default ExerciseDetails;
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom'; // CHANGES!!
 import { useDispatch, useSelector } from 'react-redux';
 import { editExercise } from 'app/actions/exercise';
 import Button from 'components/Button/Button';
@@ -344,10 +545,11 @@ import NotificationSnackbar from '../NotificationSnackbar';
 
 const ExerciseDetails = ({ action }) => {
   const { id } = useParams();
+  const navigate = useNavigate(); // CHANGES!!
   const entity = useSelector(state => state.exercises.exercises.find(e => e.id === parseInt(id, 10)));
   const dispatch = useDispatch();
   const [isEditing, setIsEditing] = useState(action === 'edit');
-  const [notification, setNotification] = useState({ open: false, message: '', severity: '' }); // Добавляем состояние уведомлений
+  const [notification, setNotification] = useState({ open: false, message: '', severity: '' });
 
   const [formData, setFormData] = useState({
     topic: '',
@@ -406,11 +608,11 @@ const ExerciseDetails = ({ action }) => {
     dispatch(editExercise(updatedEntity))
       .then(() => {
         setIsEditing(false);
-        setNotification({ open: true, message: 'Зміни збережено', severity: 'success' }); // Устанавливаем уведомление
+        setNotification({ open: true, message: 'Зміни збережено', severity: 'success' });
       })
       .catch(error => {
         console.error('Failed to save exercise:', error);
-        setNotification({ open: true, message: 'Помилка при збереженні', severity: 'error' }); // Устанавливаем уведомление об ошибке
+        setNotification({ open: true, message: 'Помилка при збереженні', severity: 'error' });
       });
   };
 
@@ -449,6 +651,10 @@ const ExerciseDetails = ({ action }) => {
     setNotification({ ...notification, open: false });
   };
 
+  const handleBack = () => { // CHANGES!!
+    navigate(-1); // CHANGES!!
+  };
+
   if (id && !entity) {
     return <div>Задачу не знайдено</div>;
   }
@@ -456,6 +662,7 @@ const ExerciseDetails = ({ action }) => {
   return (
     <div className="exercise-details">
       <div className="content-container" style={{ position: 'relative' }}>
+        <Button onClick={handleBack}>Назад</Button> {/* CHANGES!! */}
         <div>
           {isEditing ? (
             <div>
--- src/pages/exercisesList/containers/ExercisesList.jsx
+++ src/pages/exercisesList/containers/ExercisesList.jsx
@@ -119,8 +119,274 @@
 // export default ExercisesList;
 
 
-import { useIntl } from 'react-intl';
-import Typography from 'components/Typography';
+// import { useIntl } from 'react-intl';
+// import Typography from 'components/Typography';
+// import React, { useEffect, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { fetchExercises, deleteExercise } from '../../../app/actions/exercise';
+// import IconButton from 'components/IconButton/IconButton';
+// import TrashBinIcon from 'pages/exercisesList/components/icons/TrashBinIcon';
+// import DeleteConfirmationDialog from '../components/DeleteConfirmationDialog';
+// import NotificationSnackbar from '../components/NotificationSnackbar';
+// import { Link, useNavigate } from 'react-router-dom'; // CHANGES!!
+// import pages from 'constants/pagesURLs';
+// import Button from 'components/Button/Button'; // CHANGES!!
+
+// function ExercisesList() {
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate(); // CHANGES!!
+//   const { exercises, isLoading, error } = useSelector((state) => state.exercises);
+//   const [hoveredExercise, setHoveredExercise] = useState(null);
+
+//   const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+//   const [selectedExercise, setSelectedExercise] = useState(null);
+
+//   const [snackbarOpen, setSnackbarOpen] = useState(false);
+//   const [snackbarMessage, setSnackbarMessage] = useState('');
+//   const [snackbarSeverity, setSnackbarSeverity] = useState('success');
+//   const [loading, setLoading] = useState(false);
+//   const [errorMessage, setErrorMessage] = useState('');
+
+//   useEffect(() => {
+//     dispatch(fetchExercises());
+//   }, [dispatch]);
+
+//   const handleDeleteClick = (exercise) => {
+//     setSelectedExercise(exercise);
+//     setShowDeleteDialog(true);
+//   };
+
+//   const handleCancelDelete = () => {
+//     setShowDeleteDialog(false);
+//     setErrorMessage('');
+//   };
+
+//   const handleConfirmDelete = () => {
+//     if (selectedExercise) {
+//       setLoading(true);
+//       dispatch(deleteExercise(selectedExercise.id))
+//         .then(() => {
+//           setShowDeleteDialog(false);
+//           setSnackbarMessage('Видалено успішно');
+//           setSnackbarSeverity('success');
+//           setSnackbarOpen(true);
+//         })
+//         .catch((error) => {
+//           setErrorMessage('Вибачте, сталася помилка');
+//         })
+//         .finally(() => {
+//           setLoading(false);
+//         });
+//     }
+//   };
+
+//   const handleSnackbarClose = (event, reason) => {
+//     if (reason === 'clickaway') {
+//       return;
+//     }
+//     setSnackbarOpen(false);
+//   };
+
+//   const handleAddEntityClick = () => { // CHANGES!!
+//     navigate(`${pages.exercise}/new`); // CHANGES!!
+//   }; // CHANGES!!
+
+//   if (isLoading && showDeleteDialog !== true) {
+//     return <div>Loading...</div>;
+//   }
+
+//   // if (error) {
+//   //   return <div>Error: {error.message}</div>;
+//   // }
+
+//   return (
+//     <div>
+//       <Button onClick={handleAddEntityClick}>Додати задачу</Button> {/* CHANGES!! */}
+//       <ul>
+//         {exercises.map((exercise) => (
+//           <li
+//             key={exercise.id}
+//             onMouseEnter={() => setHoveredExercise(exercise)}
+//             onMouseLeave={() => setHoveredExercise(null)}
+//           >
+//             <Link to={`${pages.exercise}/${exercise.id}`}>
+//               {exercise.topic} (Рівень складності: {exercise.difficultyRange})
+//             </Link>
+//             {hoveredExercise === exercise && (
+//               <IconButton aria-label="delete" onClick={() => handleDeleteClick(exercise)}>
+//                 <TrashBinIcon />
+//               </IconButton>
+//             )}
+//           </li>
+//         ))}
+//       </ul>
+//       <DeleteConfirmationDialog
+//         open={showDeleteDialog}
+//         onClose={handleCancelDelete}
+//         onConfirm={handleConfirmDelete}
+//         loading={loading}
+//         errorMessage={errorMessage}
+//       />
+//       <NotificationSnackbar
+//         open={snackbarOpen}
+//         message={snackbarMessage}
+//         severity={snackbarSeverity}
+//         onClose={handleSnackbarClose}
+//       />
+//     </div>
+//   );
+// }
+
+// export default ExercisesList;
+
+// import React, { useEffect, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { fetchExercises, deleteExercise } from '../../../app/actions/exercise';
+// import IconButton from 'components/IconButton/IconButton';
+// import TrashBinIcon from 'pages/exercisesList/components/icons/TrashBinIcon';
+// import DeleteConfirmationDialog from '../components/DeleteConfirmationDialog';
+// import NotificationSnackbar from '../components/NotificationSnackbar';
+// import { Link, useNavigate } from 'react-router-dom';
+// import pages from 'constants/pagesURLs';
+// import Button from 'components/Button/Button';
+
+// function ExercisesList() {
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+//   const { exercises, isLoading, error } = useSelector((state) => state.exercises);
+//   const [hoveredExercise, setHoveredExercise] = useState(null);
+
+//   const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+//   const [selectedExercise, setSelectedExercise] = useState(null);
+
+//   const [snackbarOpen, setSnackbarOpen] = useState(false);
+//   const [snackbarMessage, setSnackbarMessage] = useState('');
+//   const [snackbarSeverity, setSnackbarSeverity] = useState('success');
+//   const [loading, setLoading] = useState(false);
+//   const [errorMessage, setErrorMessage] = useState('');
+
+//   // Додали стан для відстеження поточної сторінки
+//   const [currentPage, setCurrentPage] = useState(1);
+//   // Кількість елементів на сторінці
+//   const itemsPerPage = 10;
+
+//   useEffect(() => {
+//     dispatch(fetchExercises());
+//   }, [dispatch]);
+
+//   const handleDeleteClick = (exercise) => {
+//     setSelectedExercise(exercise);
+//     setShowDeleteDialog(true);
+//   };
+
+//   const handleCancelDelete = () => {
+//     setShowDeleteDialog(false);
+//     setErrorMessage('');
+//   };
+
+//   const handleConfirmDelete = () => {
+//     if (selectedExercise) {
+//       setLoading(true);
+//       dispatch(deleteExercise(selectedExercise.id))
+//         .then(() => {
+//           setShowDeleteDialog(false);
+//           setSnackbarMessage('Видалено успішно');
+//           setSnackbarSeverity('success');
+//           setSnackbarOpen(true);
+//         })
+//         .catch((error) => {
+//           setErrorMessage('Вибачте, сталася помилка');
+//         })
+//         .finally(() => {
+//           setLoading(false);
+//         });
+//     }
+//   };
+
+//   const handleSnackbarClose = (event, reason) => {
+//     if (reason === 'clickaway') {
+//       return;
+//     }
+//     setSnackbarOpen(false);
+//   };
+
+//   const handleAddEntityClick = () => {
+//     navigate(`${pages.exercise}/new`);
+//   };
+
+//   const handlePageChange = (pageNumber) => {
+//     setCurrentPage(pageNumber);
+//   };
+
+//   if (isLoading && showDeleteDialog !== true) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (error) {
+//     return <div>Error: {error.message}</div>;
+//   }
+
+//   // Розрахунок індексів початку і кінця для відображення елементів поточної сторінки
+//   const indexOfLastItem = currentPage * itemsPerPage;
+//   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+//   const currentItems = exercises.slice(indexOfFirstItem, indexOfLastItem);
+
+//   return (
+//     <div>
+//       <Button onClick={handleAddEntityClick}>Додати задачу</Button>
+//       <ul>
+//         {currentItems.map((exercise) => (
+//           <li
+//             key={exercise.id}
+//             onMouseEnter={() => setHoveredExercise(exercise)}
+//             onMouseLeave={() => setHoveredExercise(null)}
+//           >
+//             <Link to={`${pages.exercise}/${exercise.id}`}>
+//               {exercise.topic} (Рівень складності: {exercise.difficultyRange})
+//             </Link>
+//             {hoveredExercise === exercise && (
+//               <IconButton aria-label="delete" onClick={() => handleDeleteClick(exercise)}>
+//                 <TrashBinIcon />
+//               </IconButton>
+//             )}
+//           </li>
+//         ))}
+//       </ul>
+
+//       {/* Додали кнопки пагінації */}
+//       <div>
+//         <Button disabled={currentPage === 1} onClick={() => handlePageChange(currentPage - 1)}>
+//           Попередня
+//         </Button>
+//         <span>{`Сторінка ${currentPage}`}</span>
+//         <Button
+//           disabled={currentItems.length < itemsPerPage}
+//           onClick={() => handlePageChange(currentPage + 1)}
+//         >
+//           Наступна
+//         </Button>
+//       </div>
+
+//       <DeleteConfirmationDialog
+//         open={showDeleteDialog}
+//         onClose={handleCancelDelete}
+//         onConfirm={handleConfirmDelete}
+//         loading={loading}
+//         errorMessage={errorMessage}
+//       />
+//       <NotificationSnackbar
+//         open={snackbarOpen}
+//         message={snackbarMessage}
+//         severity={snackbarSeverity}
+//         onClose={handleSnackbarClose}
+//       />
+//     </div>
+//   );
+// }
+
+// export default ExercisesList;
+
+
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchExercises, deleteExercise } from '../../../app/actions/exercise';
@@ -128,13 +394,13 @@ import IconButton from 'components/IconButton/IconButton';
 import TrashBinIcon from 'pages/exercisesList/components/icons/TrashBinIcon';
 import DeleteConfirmationDialog from '../components/DeleteConfirmationDialog';
 import NotificationSnackbar from '../components/NotificationSnackbar';
-import { Link, useNavigate } from 'react-router-dom'; // CHANGES!!
+import { Link, useNavigate } from 'react-router-dom';
 import pages from 'constants/pagesURLs';
-import Button from 'components/Button/Button'; // CHANGES!!
+import Button from 'components/Button/Button';
 
 function ExercisesList() {
   const dispatch = useDispatch();
-  const navigate = useNavigate(); // CHANGES!!
+  const navigate = useNavigate();
   const { exercises, isLoading, error } = useSelector((state) => state.exercises);
   const [hoveredExercise, setHoveredExercise] = useState(null);
 
@@ -147,9 +413,16 @@ function ExercisesList() {
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
 
+  const [currentPage, setCurrentPage] = useState(parseInt(sessionStorage.getItem('currentPage')) || 1);
+  const itemsPerPage = 10;
+
   useEffect(() => {
     dispatch(fetchExercises());
   }, [dispatch]);
+
+  useEffect(() => {
+    sessionStorage.setItem('currentPage', currentPage);
+  }, [currentPage]);
 
   const handleDeleteClick = (exercise) => {
     setSelectedExercise(exercise);
@@ -187,23 +460,31 @@ function ExercisesList() {
     setSnackbarOpen(false);
   };
 
-  const handleAddEntityClick = () => { // CHANGES!!
-    navigate(`${pages.exercise}/new`); // CHANGES!!
-  }; // CHANGES!!
+  const handleAddEntityClick = () => {
+    navigate(`${pages.exercise}/new`);
+  };
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   if (isLoading && showDeleteDialog !== true) {
     return <div>Loading...</div>;
   }
 
-  // if (error) {
-  //   return <div>Error: {error.message}</div>;
-  // }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = exercises.slice(indexOfFirstItem, indexOfLastItem);
 
   return (
     <div>
-      <Button onClick={handleAddEntityClick}>Додати задачу</Button> {/* CHANGES!! */}
+      <Button onClick={handleAddEntityClick}>Додати задачу</Button>
       <ul>
-        {exercises.map((exercise) => (
+        {currentItems.map((exercise) => (
           <li
             key={exercise.id}
             onMouseEnter={() => setHoveredExercise(exercise)}
@@ -220,6 +501,20 @@ function ExercisesList() {
           </li>
         ))}
       </ul>
+
+      <div>
+        <Button disabled={currentPage === 1} onClick={() => handlePageChange(currentPage - 1)}>
+          Попередня
+        </Button>
+        <span>{`Сторінка ${currentPage}`}</span>
+        <Button
+          disabled={currentItems.length < itemsPerPage}
+          onClick={() => handlePageChange(currentPage + 1)}
+        >
+          Наступна
+        </Button>
+      </div>
+
       <DeleteConfirmationDialog
         open={showDeleteDialog}
         onClose={handleCancelDelete}
@@ -238,4 +533,3 @@ function ExercisesList() {
 }
 
 export default ExercisesList;
-
