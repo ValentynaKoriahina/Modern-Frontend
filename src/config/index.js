f255fb4b0d5b780218ccff24401bc76d917c6558
ValentynaKoriahina <80168121+ValentynaKoriahina@users.noreply.github.com>
2024-06-30 14:36:26 +0100

    EXERCISES_SERVICE

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
   // Services
-  EXERCISES_SERVICE: "http://localhost:8080",
+  EXERCISES_SERVICE: "https://chess-2-load-balancer-891026373.eu-north-1.elb.amazonaws.com",
   USERS_SERVICE: 'http://localhost:3000',
   UI_URL_PREFIX: process.env.REACT_APP_UI_URL_PREFIX || '',
 };
